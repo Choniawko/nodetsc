385d51635ebacff545d9a10536b79662021d0d81
choniawko <pawel.choniawko@gmail.com>
2017-02-11 17:10:25 +0100

    init node server with typescript

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+export class Server {
+    private app: express.Express;
+
+    constructor() {
+        this.app = express();
+     }
+
+     public setRoutes() {
+         this.app.get("/", (req: express.Request, res: express.Response) => {
+            res.send("Server run!");
+         });
+     }
+
+     public startServer() {
+         this.app.listen(3001, () => {
+             console.log("Aplication listening on 3001");
+         });
+     }
+}
