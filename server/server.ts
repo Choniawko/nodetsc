8a180781eff4b08d962e9c560572420f97a0ce23
choniawko <pawel.choniawko@gmail.com>
2017-02-12 17:30:40 +0100

    socket listen

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,41 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as socketIO from "socket.io";
 
 export class Server {
     private app: express.Express;
+    private server: express.Express;
+    private io: SocketIO.Server;
 
     constructor() {
         this.app = express();
+        this.server = require("http").Server(this.app);
+        this.io = socketIO(this.server);
         this.setRoutes();
      }
 
-     bootstrap() {
+     public bootstrap(): Server {
          return new Server();
      }
 
-     public setRoutes() {
+     public setRoutes(): void {
          this.app.get("/", (req: express.Request, res: express.Response) => {
             res.send("Server run!");
          });
      }
 
-     public startServer() {
-         this.app.listen(3001, () => {
+     public startServer(): void {
+         this.server.listen(3001, () => {
              console.log("Aplication listening on 3001");
          });
+         this.listenSocket();
+     }
+
+     private listenSocket(): void {
+        this.io.on("connection", (socket) => {
+            socket.on("message", (msg: string) => {
+                socket.emit("message", msg);
+            });
+        });
      }
 }
